809e481673541a0c2cfc3282e593f01836732d77
Jibon Roy <jsroy5796@gmail.com>
2025-08-26 23:59:06 +0600

    Add files via upload

---
+++ project.js
@@ -0,0 +1,23 @@
+function showUp(){
+    alert("Message Done!")
+}
+
+function showUp1(){
+    alert("Request accept!")
+}
+
+function linkAdd(){
+    alert("Updating soon")
+}
+
+function linkAdd1(){
+    alert("Updating soon")
+}
+
+function linkAdd2(){
+    alert("Updating soon")
+}
+
+function linkByIdPush(){
+    alert("Updating soon")
+}
